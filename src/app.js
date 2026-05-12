a82aac1ab642f3dec96df5245d9796df5b8b1d72
abdulrehmanazizf5 <abdulrehmanaziz5f@gmail.com>
2026-05-12 15:44:50 +0500

    feat: initialize express application with middleware, rate limiting, and authentication routes

--- src/app.js
+++ src/app.js
@@ -4,10 +4,18 @@ import cors from "cors";
 import authRoutes from "./routes/auth.routes.js";
 import errorHandler from "./middleware/error.middleware.js";
 import cookieParser from "cookie-parser";
+import rateLimit from "express-rate-limit";
+
+const limiter = rateLimit({
+  windowMs: 60 * 1000,
+  max: 100,
+  message: "Too many requests from this IP, please try again later",
+});
 
 dotenv.config();
 
 const app = express();
+app.use(limiter);
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
