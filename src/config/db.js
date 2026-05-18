a11bdf594c90d2af308886952ddb0b7b4ca67ee5
ABDUL REHMAN AZIZ SHEIKH <abdulrehmanazizsheikh@gmail.com>
2026-05-18 10:52:17 +0500

    .

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,15 @@
 import mongoose from "mongoose";
+let isConnected = false;
+
 
 const connectDB = async()=>{
     try{
+        if (isConnected) {
+            return;
+        }
         const connectionInstance = await mongoose.connect(process.env.MONGODB_URI)
         console.log("MongoDB connected", connectionInstance.connection.host)
+        isConnected = true
     }
     catch(err){
         console.log("Error connecting to MongoDB", err)
